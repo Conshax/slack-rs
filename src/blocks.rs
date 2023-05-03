9727830d449d0c4853eb29265df25c1f04f43d0e
Alwin <alwin@conshax.com>
2023-05-03 09:29:33 +0200

    SImpleSlackMessage

--- src/blocks.rs
+++ src/blocks.rs
@@ -2,8 +2,13 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct SlackMessage {
+pub struct SimpleSlackMessage {
     pub text: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SlackMessage {
     pub blocks: Vec<Block>,
 }
 
